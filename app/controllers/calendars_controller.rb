7970a6eeaf6d58de8e3ea6977f5c264ee9a3e86a
yhirokane17 <131893435+yhirokane17@users.noreply.github.com>
2023-05-16 16:06:39 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,15 +7,15 @@ class CalendarsController < ApplicationController
   end
 
   # 予定の保存
-  def calender_params
-    params.require(:calendars).permit(:plan, :date)
+  def create
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -37,7 +37,7 @@ class CalendarsController < ApplicationController
 
       wday_num = (@todays_date + x).wday
       if wday_num >=7
-        wday_num -=7
+        wday_num = wday_num-7
       end
 
       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wday_num}
